1d76a2e13048658668e4555feca2112b98c23757
jarierca <javialiaga2000@gmail.com>
2024-07-26 00:33:04 +0200

    formatting enconding

---
+++ src/data/encodingOptions.js
@@ -0,0 +1,34 @@
+// encodingOptions.js
+const encodingOptions = [
+  { value: "ISO-8859-1", label: "ISO-8859-1 (Latin Alphabet No. 1)" },
+  { value: "ISO-8859-2", label: "ISO-8859-2 (Latin Alphabet No. 2)" },
+  { value: "ISO-8859-3", label: "ISO-8859-3 (Latin Alphabet No. 3)" },
+  { value: "ISO-8859-4", label: "ISO-8859-4 (Latin Alphabet No. 4)" },
+  { value: "ISO-8859-5", label: "ISO-8859-5 (Latin/Cyrillic Alphabet)" },
+  { value: "ISO-8859-6", label: "ISO-8859-6 (Latin/Arabic Alphabet)" },
+  { value: "ISO-8859-7", label: "ISO-8859-7 (Latin/Greek Alphabet)" },
+  { value: "ISO-8859-8", label: "ISO-8859-8 (Latin/Hebrew Alphabet)" },
+  { value: "ISO-8859-9", label: "ISO-8859-9 (Latin Alphabet No. 5)" },
+  { value: "ISO-8859-13", label: "ISO-8859-13 (Latin Alphabet No. 7)" },
+  { value: "ISO-8859-15", label: "ISO-8859-15 (Latin Alphabet No. 9)" },
+  { value: "UTF-8", label: "UTF-8" },
+  { value: "UTF-16", label: "UTF-16" },
+  { value: "UTF-16BE", label: "UTF-16 (Big-Endian)" },
+  { value: "UTF-16LE", label: "UTF-16 (Little-Endian)" },
+  { value: "UTF-32", label: "UTF-32" },
+  { value: "UTF-32BE", label: "UTF-32 (Big-Endian)" },
+  { value: "UTF-32LE", label: "UTF-32 (Little-Endian)" },
+  { value: "US-ASCII", label: "US-ASCII" },
+  { value: "windows-1250", label: "windows-1250 (Windows Eastern European)" },
+  { value: "windows-1251", label: "windows-1251 (Windows Cyrillic)" },
+  { value: "windows-1252", label: "windows-1252 (Windows Latin-1)" },
+  { value: "windows-1253", label: "windows-1253 (Windows Greek)" },
+  { value: "windows-1254", label: "windows-1254 (Windows Turkish)" },
+  { value: "windows-1255", label: "windows-1255 (Windows Hebrew)" },
+  { value: "windows-1256", label: "windows-1256 (Windows Arabic)" },
+  { value: "windows-1257", label: "windows-1257 (Windows Baltic)" },
+  { value: "windows-1258", label: "windows-1258 (Windows Vietnamese)" },
+];
+
+export default encodingOptions;
+
