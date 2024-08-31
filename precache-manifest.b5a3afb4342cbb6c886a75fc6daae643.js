9c96340834fc68027ef270c595c30b84c54254fd
jarierca <jarierca@users.noreply.github.com>
2024-08-31 21:27:30 +0000

    deploy: fee9a3f1b6fcae237366ebef17d483088a71e2f3

---
+++ precache-manifest.b5a3afb4342cbb6c886a75fc6daae643.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e8a45b89197042ca9a5ef82a7227302",
+    "url": "/daily-toolset/index.html"
+  },
+  {
+    "revision": "abef187ea58c5b894313",
+    "url": "/daily-toolset/static/css/main.2462e81c.chunk.css"
+  },
+  {
+    "revision": "ee731a986cf6bbedab58",
+    "url": "/daily-toolset/static/js/2.b190f7d7.chunk.js"
+  },
+  {
+    "revision": "abef187ea58c5b894313",
+    "url": "/daily-toolset/static/js/main.d86b9c9c.chunk.js"
+  },
+  {
+    "revision": "6cd2b906bd19f00c9376",
+    "url": "/daily-toolset/static/js/runtime~main.add72a2e.js"
+  },
+  {
+    "revision": "e3e1708f0f325c901019010c0e29e2e9",
+    "url": "/daily-toolset/static/media/heads.e3e1708f.png"
+  },
+  {
+    "revision": "9f9af0a722dc7da04263efd0dec18938",
+    "url": "/daily-toolset/static/media/tails.9f9af0a7.png"
+  }
+]);
